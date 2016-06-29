4009f0a012575271d4c3e26a7d916dc61a536738
Peter Maneykowski <pmaneyko@gmail.com>
2016-06-28 20:42:44 -0500

    changed install script

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+cd ~/
+
+mv ~/dotfiles/ ~/.dotfiles/ || exit 1
+
 files="\
-bash/bashrc\
-bash/bash_profile\
-bash/inputrc\
-tmux/tmux.conf\
-vim/vimrc
+.dotfiles/bash/
+.dotfiles/bin/
+.dotfiles/config/
+.dotfiles/tmux/
+.dotfiles/vim/
+.bash/bashrc
+.bash/bash_profile
+.bash/inputrc
+.tmux/tmux.conf
+.vim/vimrc
 "
+
+mkdir .dotfiles/backup/
+for f in $files; do
+  ln -s $f . || \
+    (mv .`basename $f` ~/dotfiles/backup/`basename $f` && ln -s $f .)
+done
+
+if [[ "$1" != +(-p|--plugins) ]]; then
+  vim +PluginInstall
+fi
 

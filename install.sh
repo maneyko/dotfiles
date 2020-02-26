7213a6e05093cd8a2f10899c155689bf6b5a5f92
Peter Maneykowski <pmaneyko@gmail.com>
2020-02-26 16:30:11 -0600

    update install and bash-http-server

--- install.sh
+++ install.sh
@@ -88,10 +88,13 @@ do
     curl -fLo $HOME/.vim/autoload/plug.vim \
       --create-dirs \
       'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim \
-      --create-dirs \
-        'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     vim +PlugInstall +qall
-    test -n "$(command -v nvim)" && nvim +PlugInstall +qall
+    if test -n "$(command -v nvim)"
+    then
+      curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim \
+        --create-dirs \
+          'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+      nvim +PlugInstall +qall
+    fi
   fi
 done

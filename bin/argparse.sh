f9b9a57415beba4b81bb74e17b262fa512987092
Peter Maneykowski <pmaneyko@gmail.com>
2020-07-09 17:17:05 -0500

    add argparse and fix backup-path

---
+++ bin/argparse.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+
+# argparse.sh
+#
+# Place this file in your $PATH and source it using:
+#   source "argparse.sh"
+#
+# Or if argparse.sh is in the same directory as your script, you may use:
+#   source "$(dirname "$0")/argparse.sh"
+#
+# Your script may still be called from anywhere in the filesystem and the relative
+# source will evaluate correctly.
+#
+# Add it to the top of your script as if it is a library you are using in a programming
+# language such as Python.
+#
+# The functions the user is supposed to use when sourcing this file in their script are:
+#
+# * arg_positional
+#   - This parses positional arguments to your script
+#   - Usage example: arg_positional "[infile] [The input file]"
+#   - CLI example: ./myscript.sh myfile.txt
+#     The value will be accessible to you via `$ARG_INFILE` after calling `parse_args`.
+#
+# * arg_optional
+#   - This parses optional flags that take a corresponding value
+#   - Usage example: arg_optional "[port] [p] [The port to use]"
+#   - CLI example: ./myscript.sh --port 8080
+#     The value will be accessible to you via `$ARG_PORT` after calling `parse_args`.
+#     If the flag is not used, the variable `$ARG_PORT` will not be set.
+#   - CLI example: ./myscript.sh -p8080
+#     Notice there is no space between the flag and the variable, argparse.sh will parse
+#     this correctly and `$ARG_PORT` will be set to 8080.
+#
+# * arg_boolean
+#   - This parses optional flags that indicate "true" by their presence
+#   - Usage example: arg_boolean "[verbose] [v] [Do verbose output]"
+#   - CLI example: ./myscript.sh -v
+#     The value will be accessible to you via `$ARG_VERBOSE` after calling `parse_args`.
+#     If the flag is not used, the variable `$ARG_VERBOSE` will not be set.
+#
+# * arg_help
+#   - This is optional and will add the '-h' and '--help' flags as arguments to your script.
+#   - Usage example: arg_help "[My custom help message]"
+#   - CLI example: ./myscript.sh -h
+#     All the commands you registered with argparse.sh will be printed to the console in a smart way.
+#
+# * parse_args
+#   - This is a required step and must be run after registering all your variables with argparse.sh.
+
+
+ARGS_ARR=("$@")
+MAIN_FILE=$0
+
+POSITIONAL_NAMES=()
+POSITIONAL_DESCRIPTIONS=()
+
+BOOLEAN_NAMES=()
+BOOLEAN_FLAGS=()
+BOOLEAN_DESCRIPTIONS=()
+
+OPTIONAL_NAMES=()
+OPTIONAL_FLAGS=()
+OPTIONAL_DESCRIPTIONS=()
+
+HELP_DESCRIPTION=
+
+clr() {  # (number, text)
+  printf "\033[38;5;${1}m${2}\033[0m"
+}
+
+parse_arg1() {
+  t1="${1%%\]*}"
+  parse_arg1_result="${t1#*\[}"
+}
+
+parse_arg2() {
+  t1="${1#*[ ]}"
+  parse_arg1 "$t1"
+  parse_arg2_result="$parse_arg1_result"
+}
+
+parse_arg3() {
+  t1="${1##*\[}"
+  parse_arg3_result="${t1%\]}"
+}
+
+# @param arg_name
+# @param arg_description
+arg_positional() {
+  parse_arg1 "$1"
+  arg_name="$parse_arg1_result"
+  parse_arg3 "$1"
+  arg_desc="$parse_arg3_result"
+  POSITIONAL_NAMES+=($arg_name)
+  POSITIONAL_DESCRIPTIONS+=("$arg_desc")
+}
+
+# @param arg_name
+# @param arg_flag
+# @param arg_description
+arg_optional() {
+  parse_arg1 "$1"
+  arg_name="$parse_arg1_result"
+  parse_arg2 "$1"
+  arg_flag="$parse_arg2_result"
+  parse_arg3 "$1"
+  arg_desc="$parse_arg3_result"
+  OPTIONAL_NAMES+=($arg_name)
+  OPTIONAL_FLAGS+=($arg_flag)
+  OPTIONAL_DESCRIPTIONS+=("$arg_desc")
+}
+
+# @param arg_name
+# @param arg_flag
+# @param arg_description
+arg_boolean() {
+  parse_arg1 "$1"
+  arg_name="$parse_arg1_result"
+  parse_arg2 "$1"
+  arg_flag="$parse_arg2_result"
+  parse_arg3 "$1"
+  arg_desc="$parse_arg3_result"
+  BOOLEAN_NAMES+=($arg_name)
+  BOOLEAN_FLAGS+=($arg_flag)
+  BOOLEAN_DESCRIPTIONS+=("$arg_desc")
+}
+
+# @param arg_description
+arg_help() {
+  t1="${1#*\[}"
+  HELP_DESCRIPTION="${t1%\]*}"
+  BOOLEAN_NAMES+=("help")
+  BOOLEAN_FLAGS+=("h")
+  BOOLEAN_DESCRIPTIONS+=("Print this help message.")
+}
+
+parse_args() {
+  parse_args2 "${ARGS_ARR[@]}"
+}
+
+# @param args_arr
+parse_args2() {
+  POSITIONAL=()
+  while test $# -gt 0; do
+    key=$1
+    found=
+    i=0
+    for opt_name in "${OPTIONAL_NAMES[@]}"; do
+      opt_flag="${OPTIONAL_FLAGS[$i]}"
+      i=$(($i+1))
+      case $key in
+        -$opt_flag*|--$opt_name)
+          name_upper="$(echo $opt_name | tr '/a-z/' '/A-Z/' | tr '-' '_')"
+          if [[ $key =~ ^-$opt_flag ]]; then
+            if [[ $key == -$opt_flag ]]; then
+              val="$2"
+              shift; shift
+            else
+              val="${key/-"$opt_flag"}"
+              shift
+            fi
+          else
+            val="$2"
+            shift; shift
+          fi
+          eval "$(printf "ARG_$name_upper=\"$val\"")"
+          found=1
+          ;;
+      esac
+    done
+    i=0
+    for opt_name in "${BOOLEAN_NAMES[@]}"; do
+      opt_flag="${BOOLEAN_FLAGS[$i]}"
+      i=$(($i+1))
+      case $key in
+        -$opt_flag|--$opt_name)
+          name_upper="$(echo $opt_name | tr '/a-z/' '/A-Z/' | tr '-' '_')"
+          eval "$(printf "ARG_$name_upper=true")"
+          found=1
+          shift
+          ;;
+      esac
+    done
+    if test -z "$found"; then
+      POSITIONAL+=("$1")
+      shift
+    fi
+  done
+  set -- "${POSITIONAL[@]}"
+
+  i=0
+  for name in "${POSITIONAL_NAMES[@]}"; do
+    name_upper="$(echo $name | tr '/a-z/' '/A-Z/' | tr '-' '_')"
+    eval "$(printf "ARG_$name_upper=${POSITIONAL[$i]}")"
+    i=$(($i+1))
+  done
+
+  if test -n "$ARG_HELP"; then
+    print_help
+    exit 0
+  fi
+}
+
+print_help() {
+  printf "usage:  `basename $MAIN_FILE` "
+  for p_name in "${POSITIONAL_NAMES[@]}"; do
+    printf "[$p_name] "
+  done
+  for bool_flag in "${BOOLEAN_FLAGS[@]}"; do
+    printf "[-$bool_flag] "
+  done
+  i=0
+  for opt_name in "${OPTIONAL_NAMES[@]}"; do
+    opt_flag="${OPTIONAL_FLAGS[$i]}"
+    printf "[-$opt_flag $opt_name] "
+    i=$(($i+1))
+  done
+  printf "\n\n$HELP_DESCRIPTION\n\n"
+  if test -n "${POSITIONAL_NAMES}"; then
+    printf "positional arguments:\n"
+    i=0
+    for p_name in "${POSITIONAL_NAMES[@]}"; do
+      p_disp="$(clr 3 "$p_name")"
+      printf "  %-37s ${POSITIONAL_DESCRIPTIONS[$i]}\n" "$p_disp"
+      i=$(($i+1))
+    done
+  fi
+  if test -n "${OPTIONAL_NAMES}" -o -n "${BOOLEAN_NAMES}"; then
+    test -n "${POSITIONAL_NAMES}" && printf "\n"
+    printf "optional arguments:\n"
+    i=0
+    for bool_name in "${BOOLEAN_NAMES[@]}"; do
+      flag_disp="$(clr 3 "-${BOOLEAN_FLAGS[$i]}")"
+      name_disp="$(clr 3 "--$bool_name")"
+      printf "  %-50s ${BOOLEAN_DESCRIPTIONS[$i]}\n" "$flag_disp, $name_disp"
+      i=$(($i+1))
+    done
+    i=0
+    for opt_name in "${OPTIONAL_NAMES[@]}"; do
+      flag_disp="$(clr 3 "-${OPTIONAL_FLAGS[$i]}")"
+      name_disp="$(clr 3 "--$opt_name")"
+      printf "  %-50s ${OPTIONAL_DESCRIPTIONS[$i]}\n" "$flag_disp, $name_disp"
+      i=$(($i+1))
+    done
+  fi
+}

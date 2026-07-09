bfb9129050f32365b8023e5af158beb6c2be760b
Peter Maneykowski <pmaneyko@gmail.com>
2026-07-09 11:29:46 -0500

    update `excel-to-csv.py`

--- bin/excel-to-csv.py
+++ bin/excel-to-csv.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# /// script
+# dependencies = [
+#     "pandas", "xlrd", "openpyxl"
+# ]
+# ///
 
 """
-Convert Excel file formats to CSV. Run the following 'pip install' commands first if there are issues:
-
-    pip install -U pandas xlrd openpyxl
+Convert Excel file formats to CSV.
 """
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
 import pandas as pd
+
 
 def parse_args(opts=None):
     parser = ArgumentParser(description=__doc__,
@@ -16,12 +21,13 @@ def parse_args(opts=None):
     parser.add_argument('excel_file', help='Excel file to convert to CSV.')
     return parser.parse_args(opts)
 
+
 def main(args):
-    xls    = pd.ExcelFile(args.excel_file)
+    xls = pd.ExcelFile(args.excel_file)
     no_ext = ".".join(args.excel_file.split(".")[0:-1])
 
     read_excel_args = {'header': None, 'keep_default_na': False, 'na_filter': False, 'dtype': str}
-    write_csv_args  = {'header': False, 'index': False}
+    write_csv_args = {'header': False, 'index': False}
 
     if len(xls.sheet_names) <= 1:
         output_file = "%s.csv" % no_ext
@@ -35,6 +41,7 @@ def main(args):
             print("Writing sheet '%s' to '%s'" % (sheet_name, output_file))
             pd.read_excel(args.excel_file, **read_excel_args).to_csv(output_file, **write_csv_args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
     main(args)
